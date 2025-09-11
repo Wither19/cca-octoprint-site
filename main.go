0a9f1b628da1801f8ca8c41b0f18574ef1610d7a
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-09-11 12:39:13 +0000

    Made a JS function for calling the Go API

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	http.HandleFunc("/", httpfunctions.MainPage)
 	http.HandleFunc("/printer/{printerNumber}/", httpfunctions.PrinterStatePage)
+	http.HandleFunc("/api/thing", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world!"))
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
