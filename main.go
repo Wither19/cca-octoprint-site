f822b7332aa3e2980213a1405d54ef455b24dbe0
josephvranicar1 <33473253-josephvranicar1@users.noreply.replit.com>
2025-09-15 15:28:31 +0000

    Partially set up embedded filesystems for frontend

--- main.go
+++ main.go
@@ -3,48 +3,62 @@ package main
 import (
 	"embed"
 	"encoding/json"
+	"html/template"
 	"io/fs"
 	"jv/cca-octoprint/apifunctions"
 	"log"
 	"net/http"
+	"strings"
 )
 
+// Initializing two embedded filesystems: one for the HTML and the other for styles and scripts.
+
 //go:embed frontend/dist/*
-var staticWebFiles embed.FS
+var staticWebPages embed.FS
+
+//go:embed frontend/dist/assets/*
+var staticWebAssets embed.FS
 
 func main() {
-	webAppContents, err := fs.Sub(staticWebFiles, "dist")
+	webAppPages, err := fs.Sub(staticWebPages, "dist")
 	if err != nil {
 		log.Fatalln("Could not embed static files:", err)
 	}
 
-	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.FS(webAppContents))))
+	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.FS(webAppPages))))
+
+	webAppAssets, err := fs.Sub(staticWebAssets, "assets")
+	if err != nil {
+		log.Fatalln("Could not embed static files:", err)
+	}
+
+	http.Handle("/assets", http.StripPrefix("/assets", http.FileServer(http.FS(webAppAssets))))
 
 	http.HandleFunc("/", mainPage)
-	http.HandleFunc("/printer/{printerNumber}/", printerStatePage)
+	// http.HandleFunc("/printer/{printerNumber}/", printerStatePage)
 	http.HandleFunc("/api/thing", APITest)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-// func parseTemp(htmlName string, f template.FuncMap) *template.Template {
-// 	templateName := strings.ReplaceAll(htmlName, ".html", "")
+func parseTemp(htmlName string, f template.FuncMap) *template.Template {
+	templateName := strings.ReplaceAll(htmlName, ".html", "")
 
-// 	t := template.New(templateName)
+	t := template.New(templateName)
 
-// 	if f != nil {
-// 		t = t.Funcs(f)
-// 	}
+	if f != nil {
+		t = t.Funcs(f)
+	}
 
-// 	t = template.Must(template.ParseFS(staticWebFiles, htmlName))
+	t = template.Must(template.ParseFS(staticWebPages, htmlName))
 
-// 	return t
-// }
+	return t
+}
 
 var printers []string = []string{"a", "b", "c", "d", "e", "f"}
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	template.parseTemplate("dist/index.html", nil).Execute(w, nil)
+	parseTemp("/dist/index.html", nil).Execute(w, nil)
 }
 
 // func printerStatePage(w http.ResponseWriter, r *http.Request) {
