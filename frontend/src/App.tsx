7621545b242d3d9eeb2e0b6e3b1f9c48c7c5f633
Joseph Vranicar <josephvranicar1@gmail.com>
2025-09-15 17:28:05 -0400

    Remove back button from page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,28 +25,22 @@ function App() {
         ))}
       </div>
 
-      <div className="mt-3 mx-4 position-relative">
-        <a href="/">
-          <button className="btn btn-secondary">&lt;</button>
-        </a>
-      </div>
-
       {printerData ? (
         <>
-      <h2 className="display-6 text-center text-capitalize">
-        Printer {currentPrinter}
-        <span className={`badge bg-${printerData.StateColor} fs-6 position-relative`}>{printerData.State.Text}</span>
-      </h2>
-      <div id="temperature-container" className="mx-4">
-        {printerData && printerData.Temperature.map((temp: ModifiedTemperatureData) => (
-          <div id={temp.Name} className="mb-3">
-          <h5 className="text-capitalize">{temp.Name.replace("-", " ")}</h5>
-          <div>Actual: {temp.Actual} &deg;C</div>
-          <div>Target: {temp.Target} &deg;C</div>
-        </div>
-        ))}
-      </div>
-      </>
+          <h2 className="display-6 text-center text-capitalize">
+            Printer {currentPrinter}
+            <span className={`badge bg-${printerData.StateColor} fs-6 position-relative`}>{printerData.State.Text}</span>
+          </h2>
+          <div id="temperature-container" className="mx-4">
+            {printerData && printerData.Temperature.map((temp: ModifiedTemperatureData) => (
+              <div id={temp.Name} className="mb-3">
+                <h5 className="text-capitalize">{temp.Name.replace("-", " ")}</h5>
+                <div>Actual: {temp.Actual} &deg;C</div>
+                <div>Target: {temp.Target} &deg;C</div>
+              </div>
+            ))}
+          </div>
+        </>
       ) : (
         <div className="display-3 text-center">Could not reach Printer API</div>
       )}
