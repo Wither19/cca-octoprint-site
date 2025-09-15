093d8c35894994dc54f63ac789738ea209b4b2e3
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-09-15 16:05:47 +0000

    Add Bootstrap Node package and move the main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import "./App.scss"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const printerList = ["a", "b", "c", "d", "e", "f"]
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="display-6 text-center mx-2">CCA 3D Printing</div>
+    <div className="mx-3 my-5 d-flex flex-wrap justify-content-around">
+      {printerList.map((printer) => (
+         <a className="text-capitalize fs-3" href={`printer/${printer}`}>Printer {printer}</a>
+      ))}
+    </div>
     </>
   )
 }
