e4444cdd4e72a605b7fcfdda1888facddac65301
Joseph Vranicar <josephvranicar1@gmail.com>
2025-09-18 07:55:20 -0400

    Add mullish coalescing for temperature fields

--- frontend/src/components/TemperatureListItem.tsx
+++ frontend/src/components/TemperatureListItem.tsx
@@ -3,12 +3,9 @@ import type { TemperatureData } from "../types"
 function TemperatureListItem({ temperature, name }: { temperature: TemperatureData, name: string }) {
   return (
     <div id={name} className="mb-3">
-      <h5 className="text-capitalize">{name.replace("-", " ")}
-      </h5>
-      <div>Actual: {temperature["actual"]} &deg;C
-      </div>
-      <div>Target: {temperature["target"]} &deg;C
-      </div>
+      <h5 className="text-capitalize">{name.replace("-", " ")}</h5>
+      <div>Actual: {temperature["actual"] ?? 0} &deg;C</div>
+      <div>Target: {temperature["target"] ?? 0} &deg;C</div>
     </div>
   )
 }
