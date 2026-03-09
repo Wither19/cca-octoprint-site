cfea567695cb7b7994a30d996415753d25bf4316
Wither19 <146484561+Wither19@users.noreply.github.com>
2026-03-09 10:02:52 -0400

    Update TemperatureList.tsx

--- frontend/src/components/TemperatureList.tsx
+++ frontend/src/components/TemperatureList.tsx
@@ -6,8 +6,6 @@ type TemperatureListProps = {
   temperatures: Omit<PrinterStatus["temperature"], "history">
 };
 
-function TemperatureList({ temperatures }: TemperatureListProps) {
-  return Object.keys(temperatures).map((k) => <TemperatureListItem key={k} name={k.replace("tool", "tool ")} temperature={temperatures[k as keyof typeof temperatures]} />)
-}
+const TemperatureList = ({ temperatures }: TemperatureListProps) => Object.keys(temperatures).map((k) => <TemperatureListItem key={k} name={k.replace("tool", "tool ")} temperature={temperatures[k as keyof typeof temperatures]} />)
 
 export default TemperatureList
